55963436b1058cf5d959ed1c951fe58b42d1b63a
rakaanggie <rakaanggie123@gmail.com>
2023-06-05 11:20:57 +0700

    first commit

---
+++ back/s3.js
@@ -0,0 +1,35 @@
+import aws from 'aws-sdk';
+import dotenv from 'dotenv';
+import crypto from 'crypto';
+import { promisify } from 'util';
+const randomBytes = promisify(crypto.randomBytes);
+
+dotenv.config();
+
+const region = process.env.AWS_BUCKET_REGION;
+const bucketName = process.env.AWS_BUCKET_NAME;
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+
+const s3 = new aws.S3({
+    region,
+    accessKeyId,
+    secretAccessKey,
+    signatureVersion: 'v4'
+});
+
+
+export async function generateUploadURL() {
+    const rawBytes = await randomBytes(16)
+    const imageName = rawBytes.toString('hex')
+
+    const params = ({
+        Bucket: bucketName,
+        Key: imageName,
+        Expires: 60
+    })
+
+    const uploadURL = await s3.getSignedUrlPromise('putObject', params)
+    return uploadURL
+}
+
---
+++ front/index.js
@@ -0,0 +1,28 @@
+const imageForm = document.querySelector("#imageForm")
+const imageInput = document.querySelector("#imageInput")
+
+imageForm.addEventListener("submit", async event => {
+    event.preventDefault()
+    const file = imageInput.files[0]
+
+    //get secure url from server
+    const { url } = await fetch("/s3Url").then(res => res.json())
+    console.log(url)
+
+    //post the image directly to the s3 bucket
+    await fetch(url, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "multipart/form-data"
+        },
+        body: file
+    })
+
+    const imageUrl = url.split('?')[0]
+    console.log(imageUrl)
+
+    const img = document.createElement("img")
+    img.src = imageUrl
+    document.body.appendChild(img)
+})
+
